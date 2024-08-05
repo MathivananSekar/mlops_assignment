ecee85f8e28425a20eec0d359e807fd556314543
MathivananSekar <smathivanan@live.com>
2024-08-05 16:52:18 +0530

    added comments

--- model.py
+++ model.py
@@ -3,10 +3,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import joblib
 
+
 # Train model
 def train_model():
     data = load_iris()
-    X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = (
+        train_test_split(data.data, data.target, test_size=0.2,
+                         random_state=42))
     model = RandomForestClassifier()
     model.fit(X_train, y_train)
     joblib.dump(model, 'model.joblib')
